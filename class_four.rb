c95a1e702e118c5dc9bd02e6c0072e6cf0163185
inforeel <theinforeel@gmail.com>
2014-10-05 23:05:22 -0400

    This is the solutions thru the weather app, but not extra credit.

--- class_four.rb
+++ class_four.rb
@@ -206,15 +206,51 @@ strings = ["Hello", "World", "in", "inside", "a", "frame" ]
 
 
 #problem 8 Weather Stats
+#outputs the day number (column 1) with the smallest temperature spread (Max temperature is column two. Min temperature is column three).
+#first, we read the corresponding weather file. 
+# better convention in the file path bit is to not put the absolute path, but rather the relative path. Thanks @milsyobtaf.
+# for testing purposes I did puts weather_data Hooray it werks.
+#changed to .readlines to read the file in as lines
+weather_data = File.readlines("weather.txt")
+small_array = []
+master_array = []
+# need to find a way to organize hte data in the text file and push it into a new array, which could then be split up 
+#http://stackoverflow.com/questions/5809093/how-do-i-read-line-by-line-a-text-file-in-ruby-hosting-it-on-s3
+#http://stackoverflow.com/questions/5545068/what-are-all-the-common-ways-to-read-a-file-in-ruby/5545284#5545284
 
-weather_data = File.read("/Users/rebareel/bewd_2014/class_four/weather.txt")
-puts weather_data
+#weather_data.each {|line_break| org_weather_data.push (line_break.split (" "))}
 
+weather_data.each do |line|
+	small_array.push(line.split (" "))
+	# puts small_array[3]
+	# puts "-----"
+	#This says put the weather data into an array and then split up lines ONLY after a space, as otehrwise it reads characters as index positions
+	
+end
 
+#This says ok for small_array, for each element, you're going to do some math and then you're going to push it into a new array called master_array
+small_array.each do |x|	
+#master_array.push(x)
+master_array.push(x[1].to_i - x[2].to_i)
+end
 
+#janky way of getting rid of the elements in the first two positions, which were both 0 bc they were text and a blank respectively.
+master_array.shift
+master_array.shift
 
+#print master_array.min for science to see what the minimum number in the array is
 
+#This stuff basically says, set a variable best day to be the index position of the smallest value in the master array. But because we shifted those
+#first two positions, we're going to have to add one :-(
+best_day = master_array.index(master_array.min)
+print "The day in June with the smallest temperature spread is June #{best_day +1 }"
 
+	
+
+#cruft
+#spread = small_array[1].to_i - small_array[2].to_i
+# puts spread
+# small_array.delete_at(1)
 
 
 
